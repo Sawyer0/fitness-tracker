d02f8857f93d6ee918a4bf1d2342afce3563171f
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-28 08:43:19 -0400

    finished workout model this should work

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+  day: {
+    type: String,
+    default: () => new Date(),
+  },
 
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please specify resistance or cardio",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please choose an exercise name",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
